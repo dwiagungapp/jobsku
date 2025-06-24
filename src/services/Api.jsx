6ebce0ad98d8548a7c664ef4a7c361424c813eb2
unknown <dwiagungapp@gmail.com>
2025-06-24 11:53:46 +0700

    edit api ke webmaju.web.id

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "http://webmaju.web.id",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
